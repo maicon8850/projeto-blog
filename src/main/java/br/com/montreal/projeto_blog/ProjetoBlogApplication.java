5a37bb1d044335814f82bfb175ab5aa04825cb2a
maicon-douglas <maicon.douglas3527@gmail.com>
2025-03-31 23:28:44 -0300

    feat: inicia estrutura base do projeto Blog Pessoal
    - Projeto gerado com Spring Boot 3.4.4
    - Dependências: Spring Web, Spring Data JPA, Spring Security, MySQL Driver
    - Configuração inicial do pacote: br.com.montreal.projetoblog

---
+++ src/main/java/br/com/montreal/projeto_blog/ProjetoBlogApplication.java
@@ -0,0 +1,13 @@
+package br.com.montreal.projeto_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBlogApplication.class, args);
+	}
+
+}
